cc1731ea697b477c28aec1d1aad39ee84d50b1ed
王一之 <yz@ggnb.top>
2021-12-30 15:26:36 +0800

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Githubwebhook测试
+// @version      1.0.0
+// @author       test
+// @run-at       document-end
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+console.log('webhook 测试');
