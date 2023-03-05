18a6e58ef083e1a0d3325cc556bb1e1bbc9e35b6
王一之 <yz@ggnb.top>
2023-03-05 08:46:17 +0800

    Update test.user.js

--- src/test.user.js
+++ src/test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Githubwebhook测试
 // @description  test
-// @version      1.2.3
+// @version      1.2.4
 // @author       test
 // @run-at       document-end
 // @grant        unsafeWindow
